e20d08e7ec7421124b9286d68220fc4737a0ff99
Gesmerald Morales <144927240+gesmorales@users.noreply.github.com>
2026-05-02 01:36:08 +0800

    Add Flask and CORS setup to app.py

--- app.py
+++ app.py
@@ -58,3 +58,9 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
     app.run(host="0.0.0.0", port=port)
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
